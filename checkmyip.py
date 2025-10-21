5218429ea07224f3e118199f820dd2d99e1a2bf9
José Luis Bandala Pérez <luis449bp@gmail.com>
2025-10-21 15:26:44 -0600

    feat: add request-id to body feat: remove branding feat: add validations

--- checkmyip.py
+++ checkmyip.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python
 
+"""
+CheckMyIP Server
 
-#####           CheckMyIP Server             #####
-#####        Written by John W Kerns         #####
-#####       http://blog.packetsar.com        #####
-##### https://github.com/packetsar/checkmyip #####
+Written by: John W Kerns
+Website: http://blog.packetsar.com
+Repository: https://github.com/packetsar/checkmyip
+"""
 
-
-##### Inform version here #####
-import io
-
-version = "v1.4.0"
-
-##### Import python2 native modules #####
+# Import python native modules and third party modules
 import os
-import sys
 import time
 import socket
 import jinja2
 import paramiko
 import threading
+import uuid
 
-##### Jinja formatting for logging queries #####
-j2log = "Connection from: {{ ip }} ({{ port }}) ({{ proto }})"
+# Inform version here
+version = "v1.4.1"
 
-##### Jinja formatting for response queries #####
+# Jinja formatting for logging queries
+j2log = "Connection from: {{ ip }} ({{ port }}) ({{ proto }}) (request: {{ requestid }})"
+
+# Jinja formatting for response queries
 j2send = """{
 "comment": "##     Your IP Address is {{ ip }} ({{ port }})     ##",
 "family": "{{ family }}",
@@ -32,14 +31,19 @@ j2send = """{
 "port": "{{ port }}",
 "protocol": "{{ proto }}",
 "hostname": "{{ hostname }}",
+"request": "{{ requestid }}",
 "version": "%s",
-"website": "https://github.com/luisbandalap/checkmyip",
 "forked-from": "https://github.com/packetsar/checkmyip"
 }""" % version
 
 
-##### Handles all prnting to console and logging to the logfile #####
 class log_management:
+    """
+    Handles all printing to console and logging to the logfile
+    Publishes two global methods:
+        - log(data)     : Logs data to logfile and console with timestamp
+        - console(data) : Prints data to console without timestamp
+    """
     def __init__(self):
         self.logpath = "/etc/checkmyip/"  # Log file directory path
         self.logfile = "/etc/checkmyip/%scheckmyip.log" % \
@@ -56,7 +60,14 @@ class log_management:
         except IOError:
             self._create_log_dir()
 
-    def _logger(self, data):  # Logging method published to global as 'log'
+    def _logger(self, data):
+        """
+        Logging method published to global as 'log'
+        Args:
+            data (str): Data to log to logfile and console
+        Raises:
+            IOError: If unable to write to logfile
+        """
         logdata = time.strftime("%Y-%m-%d %H:%M:%S") + ":   " + data + "\n"
         if self.can_log:
             try:  # Try to write to log, create log dir if fail
@@ -74,7 +85,7 @@ class log_management:
             logdata = time.strftime("%Y-%m-%d %H:%M:%S") + ":   " + data + "\n"
         else:
             logdata = data
-        print(logdata)
+        print(logdata, flush=True)
 
     def _publish_methods(self):
         global log
@@ -82,7 +93,12 @@ class log_management:
         log = self._logger  # Global method used to write to the log file
         console = self._console  # Global method used to write to the console
 
-    def _create_log_dir(self):  # Create the directory for logging
+    def _create_log_dir(self):
+        """
+        Create the directory for logging
+        Raises:
+            Exception: If unable to create the log directory
+        """
         os.system('mkdir -p ' + self.logpath)
         self._console("Logpath (%s) created" % self.logpath)
         self.can_log = True
@@ -97,34 +113,42 @@ class log_management:
             paramiko.util.log_to_file(self.paramikolog)
 
 
-##### Creates a RSA key for use by paramiko #####
 class rsa_key:
+    """
+    RSA key class used to create a paramiko RSAKey object from a hardcoded
+    private key string.
+    Publishes a callable method that returns the paramiko.RSAKey object.
+    """
     data = """-----BEGIN RSA PRIVATE KEY-----
-	MIICWgIBAAKBgQDTj1bqB4WmayWNPB+8jVSYpZYk80Ujvj680pOTh2bORBjbIAyz
-	oWGW+GUjzKxTiiPvVmxFgx5wdsFvF03v34lEVVhMpouqPAYQ15N37K/ir5XY+9m/
-	d8ufMCkjeXsQkKqFbAlQcnWMCRnOoPHS3I4vi6hmnDDeeYTSRvfLbW0fhwIBIwKB
-	gBIiOqZYaoqbeD9OS9z2K9KR2atlTxGxOJPXiP4ESqP3NVScWNwyZ3NXHpyrJLa0
-	EbVtzsQhLn6rF+TzXnOlcipFvjsem3iYzCpuChfGQ6SovTcOjHV9z+hnpXvQ/fon
-	soVRZY65wKnF7IAoUwTmJS9opqgrN6kRgCd3DASAMd1bAkEA96SBVWFt/fJBNJ9H
-	tYnBKZGw0VeHOYmVYbvMSstssn8un+pQpUm9vlG/bp7Oxd/m+b9KWEh2xPfv6zqU
-	avNwHwJBANqzGZa/EpzF4J8pGti7oIAPUIDGMtfIcmqNXVMckrmzQ2vTfqtkEZsA
-	4rE1IERRyiJQx6EJsz21wJmGV9WJQ5kCQQDwkS0uXqVdFzgHO6S++tjmjYcxwr3g
-	H0CoFYSgbddOT6miqRskOQF3DZVkJT3kyuBgU2zKygz52ukQZMqxCb1fAkASvuTv
-	qfpH87Qq5kQhNKdbbwbmd2NxlNabazPijWuphGTdW0VfJdWfklyS2Kr+iqrs/5wV
-	HhathJt636Eg7oIjAkA8ht3MQ+XSl9yIJIS8gVpbPxSw5OMfw0PjVE7tBdQruiSc
-	nvuQES5C9BMHjF39LZiGH1iLQy7FgdHyoP+eodI7
-	-----END RSA PRIVATE KEY-----
-	"""
+    MIICWgIBAAKBgQDTj1bqB4WmayWNPB+8jVSYpZYk80Ujvj680pOTh2bORBjbIAyz
+    oWGW+GUjzKxTiiPvVmxFgx5wdsFvF03v34lEVVhMpouqPAYQ15N37K/ir5XY+9m/
+    d8ufMCkjeXsQkKqFbAlQcnWMCRnOoPHS3I4vi6hmnDDeeYTSRvfLbW0fhwIBIwKB
+    gBIiOqZYaoqbeD9OS9z2K9KR2atlTxGxOJPXiP4ESqP3NVScWNwyZ3NXHpyrJLa0
+    EbVtzsQhLn6rF+TzXnOlcipFvjsem3iYzCpuChfGQ6SovTcOjHV9z+hnpXvQ/fon
+    soVRZY65wKnF7IAoUwTmJS9opqgrN6kRgCd3DASAMd1bAkEA96SBVWFt/fJBNJ9H
+    tYnBKZGw0VeHOYmVYbvMSstssn8un+pQpUm9vlG/bp7Oxd/m+b9KWEh2xPfv6zqU
+    avNwHwJBANqzGZa/EpzF4J8pGti7oIAPUIDGMtfIcmqNXVMckrmzQ2vTfqtkEZsA
+    4rE1IERRyiJQx6EJsz21wJmGV9WJQ5kCQQDwkS0uXqVdFzgHO6S++tjmjYcxwr3g
+    H0CoFYSgbddOT6miqRskOQF3DZVkJT3kyuBgU2zKygz52ukQZMqxCb1fAkASvuTv
+    qfpH87Qq5kQhNKdbbwbmd2NxlNabazPijWuphGTdW0VfJdWfklyS2Kr+iqrs/5wV
+    HhathJt636Eg7oIjAkA8ht3MQ+XSl9yIJIS8gVpbPxSw5OMfw0PjVE7tBdQruiSc
+    nvuQES5C9BMHjF39LZiGH1iLQy7FgdHyoP+eodI7
+    -----END RSA PRIVATE KEY-----
+    """
 
-    def readlines(self):  # For use by paramiko.RSAKey.from_private_key mthd
+    def readlines(self):
+        """"For use by paramiko.RSAKey.from_private_key mthd"""
         return self.data.split("\n")
 
-    def __call__(self):  # Recursive method uses own object as arg when called
+    def __call__(self):
+        """Recursive method uses own object as arg when called"""
         return paramiko.RSAKey.from_private_key(self)
 
 
-##### Imports and modifies the ServerInterface module for use by paramiko #####
 class ssh_server(paramiko.ServerInterface):
+    """
+    Imports and modifies the ServerInterface module for use by paramiko
+    """
     def __init__(self):
         self.event = threading.Event()
 
@@ -133,10 +157,12 @@ class ssh_server(paramiko.ServerInterface):
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
-    def check_auth_none(self, username):  # Auth none method left wide open
+    def check_auth_none(self, username):
+        """Auth none method left wide open"""
         return paramiko.AUTH_SUCCESSFUL
 
-    def get_allowed_auths(self, username):  # Give no auth options
+    def get_allowed_auths(self, username):
+        """Give no auth options"""
         return 'none'
 
     def check_channel_shell_request(self, channel):
@@ -148,14 +174,14 @@ class ssh_server(paramiko.ServerInterface):
         return True
 
 
-##### Method to merge Jinja templates #####
 def j2format(j2tmp, valdict):
+    """Method to merge Jinja templates with value dictionary"""
     template = jinja2.Template(j2tmp)
     return template.render(valdict).replace("\n", "\r\n")
 
 
-##### Cleans IP addresses coming from socket library #####
 def clean_ip(addr):
+    """Cleans IP addresses coming from socket library"""
     ip = addr[0]
     port = addr[1]
     if len(ip) > 6:  # If this IP is not a super short v6 address
@@ -165,10 +191,17 @@ def clean_ip(addr):
 
 
 def get_ip_family(ip):
+    """
+    Determines if an IP address is IPv4, IPv6, or undefined
+    Args:
+        ip (str): The IP address to check
+    Returns:
+        str: "ipv4", "ipv6", or "undefined"
+    """
     def is_ipv4(s):
         try:
             return str(int(s)) == s and 0 <= int(s) <= 255
-        except:
+        except Exception:
             return False
 
     def is_ipv6(s):
@@ -178,7 +211,7 @@ def get_ip_family(ip):
             return False
         try:
             return int(s, 16) >= 0 and s[0] != '-'
-        except:
+        except Exception:
             return False
 
     if ip.count(".") == 3 and all(is_ipv4(i) for i in ip.split(".")):
@@ -187,8 +220,16 @@ def get_ip_family(ip):
         return "ipv6"
     return "undefined"
 
-##### TCP listener methods. Gets used once for each listening port #####
+
 def listener(port, talker):
+    """
+    TCP listener methods. Gets used once for each listening port
+    Args:
+        port (int): The TCP port to listen on
+        talker (function): The talker function to handle incoming connections
+    Raises:
+        Exception: If unable to bind to the specified port.
+    """
     listen_ip = ''
     listen_port = port
     buffer_size = 1024
@@ -207,21 +248,39 @@ def listener(port, talker):
         thread.start()  # Start talker thread to listen to port
 
 
-##### Telnet responder method. Is run in own thread for each telnet query #####
 def telnet_talker(client, valdict, proto="telnet"):
-    valdict.update({"proto": proto})  # Add the protocol to the value dict
+    """
+    Telnet responder method. Is run in own thread for each telnet query
+    Args:
+        client (socket): The client socket object
+        valdict (dict): The value dictionary for Jinja templating
+        proto (str): The protocol string to use in the response
+    Raises:
+        Exception: If unable to send data to the client.
+    """
+    request_id = uuid.uuid4()
+    valdict.update({"proto": proto, "requestid": request_id})  # Add protocol and requestid to dict
     log(j2format(j2log, valdict))  # Log the query to the console and logfile
     # Send the query response
     client.send(f'{j2format(j2send, valdict)}\n'.encode())
     client.close()  # Close the channel
 
 
-##### SSH responder method. Gets run in own thread for each SSH query #####
 def ssh_talker(client, valdict, proto="ssh"):
+    """
+    SSH responder method. Gets run in own thread for each SSH query
+    Args:
+        client (socket): The client socket object
+        valdict (dict): The value dictionary for Jinja templating
+        proto (str): The protocol string to use in the response
+    Raises:
+        Exception: If unable to send data to the client.
+    """
     def makefile():  # A hack to make Cisco SSH sessions work properly
         chan.makefile('rU').readline().strip('\r\n')
 
-    valdict.update({"proto": proto})
+    request_id = uuid.uuid4()
+    valdict.update({"proto": proto, "requestid": request_id})  # Add protocol and requestid to dict
     log(j2format(j2log, valdict))
     t = paramiko.Transport(client, gss_kex=True)
     t.set_gss_host(socket.getfqdn(""))
@@ -240,18 +299,29 @@ def ssh_talker(client, valdict, proto="ssh"):
     client.close()  # And kill the session
 
 
-##### HTTP responder method. Gets run in own thread for each HTTP query #####
-##### Automatically detects if client is a browser or a telnet client   #####
 def http_talker(client, valdict, proto="http"):
+    """
+    HTTP responder method. Gets run in own thread for each HTTP query
+    Automatically detects if client is a browser or a telnet client
+    Args:
+        client (socket): The client socket object
+        valdict (dict): The value dictionary for Jinja templating
+        proto (str): The protocol string to use in the response
+    Raises:
+        Exception: If unable to send data to the client.
+    """
     time.sleep(.1)  # Sleep to allow the client to send some data
     client.setblocking(0)  # Set the socket recv as non-blocking
     browser = False  # Is the client using a browser?
     raw_request = ''
+    request_id = uuid.uuid4()
+    valdict.update({"requestid": request_id})  # Add requestid to dict
+
     try:  # client.recv() will raise an error if the buffer is empty
         raw_request = client.recv(2048).decode('utf-8')  # Recieve data from the buffer (if any)
         print(raw_request)  # Print to stdout
         browser = True  # Set client browser to True
-    except:  # If buffer was empty, then like a telnet client on TCP80
+    except Exception:  # If buffer was empty, then like a telnet client on TCP80
         browser = False  # Set client browser to False
     if not browser:  # If the client is not a browser
         telnet_talker(client, valdict, "http-telnet")  # Hand to telnet_talker
@@ -282,20 +352,25 @@ def http_talker(client, valdict, proto="http"):
             hostname, tmp_port = socket.getnameinfo((x_forwarded_ips[0].strip(), 0), 0)
             valdict.update({"hostname": hostname})
 
+        valdict.update({"proto": proto})  # Add protocol to dict
         # Proceed with standard HTTP response (with headers)
-        valdict.update({"proto": proto})
         log(j2format(j2log, valdict))
         response_body_raw = j2format(j2send, valdict) + "\n"
         response_headers_raw = """HTTP/1.1 200 OK
 Content-Length: %s
 Content-Type: application/json; encoding=utf8
-Connection: close""" % str(len(response_body_raw))  # Response with headers
+X-Request-ID: %s
+Connection: close""" % (str(len(response_body_raw)), request_id)  # Response with headers
         client.send(f'{response_headers_raw}\n\n{response_body_raw}'.encode())
         client.close()
 
 
-##### Server startup method. Starts a listener thread for each TCP port #####
 def start():
+    """
+    Server startup method. Starts a listener thread for each TCP port
+    Raises:
+        Exception: If unable to start a listener on any port.
+    """
     talkers = {22: ssh_talker, 23: telnet_talker,
                80: http_talker}  # Three listeners on different ports
     for talker in talkers:
@@ -311,8 +386,22 @@ def start():
             quit()
 
 
-##### Client class to be used to make API calls to CheckMyIP server #####
 class CheckMyIP_Client:
+    """
+    CheckMyIP_Client class used to make API calls to a CheckMyIP server.
+    3 protocols are supported:
+        - "telnet" : Connect via telnet (port 23)
+        - "ssh"    : Connect via SSH (port 22)
+        - "http"   : Connect via HTTP (port 80)
+    Example usage:
+        client = CheckMyIP_Client()  # Instantiate the client
+        client.set_family("auto")    # Set the address family type
+        data = client.get()          # Get the IP information
+    Returns:
+        dict: A dictionary containing the IP information.
+    Raises:
+        Exception: If unable to connect to the server.
+    """
     def __init__(self):
         self._json = __import__('json')  # Import the JSON library
         self._socket = __import__('socket')  # Import the socket library
@@ -322,12 +411,19 @@ class CheckMyIP_Client:
         self.server = "telnetmyip.com"  # Set the default CheckMyIP server
 
     def get(self):  # Primary method to run IP check
+        """
+        Gets the IP information from the CheckMyIP server.
+        Returns:
+            dict: A dictionary containing the IP information.
+        Raises:
+            Exception: If unable to connect to the server.
+        """
         if self._af == "auto":  # If we are using an auto address family
             try:  # Try using IPv6
                 sock = self._socket.socket(self._socket.AF_INET6,
                                            self._socket.SOCK_STREAM)
                 sock.connect((self.server, 23))
-            except:  # Fall back to IPv4 if IPv6 fails
+            except Exception:  # Fall back to IPv4 if IPv6 fails
                 sock = self._socket.socket(self._socket.AF_INET,
                                            self._socket.SOCK_STREAM)
                 sock.connect((self.server, 23))
@@ -345,16 +441,21 @@ class CheckMyIP_Client:
         return self._data  # Return the JSON data
 
     def set_family(self, family):  # Method to set the IP address family
+        """
+        Sets the IP address family for the client.
+        Args:
+            family (str): The address family to use. Must be one of:
+                - "auto" : Try IPv6 first, fall back to IPv4
+                - "ipv4" : Use IPv4 only
+                - "ipv6" : Use IPv6 only
+        Raises:
+            Exception: If the provided family is not one of the allowed values.
+        """
         allowed = ["auto", "ipv4", "ipv6"]  # Allowed input values
         if family in allowed:
             self._af = family
         else:
             raise Exception("Allowed families are 'auto', 'ipv4', 'ipv6'")
-
-
-### CheckMyIP_Client Example Usage ###
-# client = CheckMyIP_Client()
-# client.get()
 
 
 if __name__ == "__main__":
